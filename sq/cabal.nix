08d18b10b1abb851e7e2e116899ac59e04ca5499
Renzo Carbonara <gnuk0001@gmail.com>
2025-11-20 15:54:31 +0000

    cabal

--- sq/cabal.nix
+++ sq/cabal.nix
@@ -2,10 +2,11 @@
 , attoparsec-aeson, attoparsec-iso8601, base, binary, bytestring
 , clock, containers, contravariant, criterion, deepseq, df1, di
 , di-core, di-df1, direct-sqlite, directory, exceptions, filepath
-, foldl, hedgehog, lib, profunctors, ref-tf, resource-pool
-, resourcet, resourcet-extra, safe-exceptions, scientific, sop-core
-, stm, streaming, tasty, tasty-hedgehog, tasty-hunit
-, template-haskell, text, time, transformers, uuid-types
+, foldl, hedgehog, integer-logarithms, lib, profunctors, ref-tf
+, resource-pool, resourcet, resourcet-extra, safe-exceptions
+, scientific, sop-core, stm, streaming, tasty, tasty-hedgehog
+, tasty-hunit, template-haskell, text, time, transformers
+, uuid-types
 }:
 mkDerivation {
   pname = "sq";
@@ -15,9 +16,9 @@ mkDerivation {
     adjunctions aeson attoparsec attoparsec-aeson attoparsec-iso8601
     base binary bytestring clock containers contravariant deepseq
     di-core di-df1 direct-sqlite directory exceptions filepath foldl
-    profunctors ref-tf resource-pool resourcet resourcet-extra
-    safe-exceptions scientific sop-core stm streaming template-haskell
-    text time transformers uuid-types
+    integer-logarithms profunctors ref-tf resource-pool resourcet
+    resourcet-extra safe-exceptions scientific sop-core stm streaming
+    template-haskell text time transformers uuid-types
   ];
   testHaskellDepends = [
     aeson async base binary bytestring df1 di di-core hedgehog ref-tf
